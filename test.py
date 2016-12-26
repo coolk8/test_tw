2754ae22895f57cf71fea1309431e7a7ece157ad
Pavel Kulikov <coolk8@gmail.com>
2016-12-27 00:54:59 +0300

    request for arma 3

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import requests
 import time
 import sys
+import json
 
-clientId = {'client_id':'68tskg1eib5q4n0kzsa2i3wl0egcow'}
-r = requests.get('https://api.twitch.tv/kraken/channels/david_toroczkai/follows',params=clientId)
-print(r.json()['_total'])
+
+
+clientId = {'client_id':'68tskg1eib5q4n0kzsa2i3wl0egcow', 'query':'arma 3', 'limit':'100'}
+r = requests.get('https://api.twitch.tv/kraken/search/channels',params=clientId)
+print(r.json())
+
+with open('file_tw.txt','w') as file:
+    json.dump(r.json(), file)
